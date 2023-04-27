5132314bb6e532ffc1c3a76effebcba008565b36
liandrapereira <pliandra.xavier@gmail.com>
2023-04-27 22:04:37 +0100

    Ficheiro main

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include "biblioteca.h"
 
-int main() {
-    printf("Hello, World!\n");
+const int MAP_HEIGHT = 25;
+const int MAP_WIDTH = 100;
+
+Jogador* jogador;
+Peca** mapa;
+
+int main(void)
+{
+    Posicao start_pos;
+    bool compatibleTerminal;
+
+    compatibleTerminal = cursesSetup();
+
+    if (compatibleTerminal)
+    {
+        srand(time(NULL));
+
+        mapa = criaMapaPecas();
+        start_pos = setupMap();
+        jogador = criaJogador(start_pos);
+
+        gameLoop();
+
+        closeGame();
+    }
+    else
+    {
+        endwin();
+    }
+
     return 0;
 }
