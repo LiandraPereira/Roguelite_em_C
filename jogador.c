6b88374a566a6c9910e74d18671124d2046399b9
liandrapereira <pliandra.xavier@gmail.com>
2023-05-14 17:09:48 +0100

    Finalizado e documentado.

--- jogador.c
+++ jogador.c
@@ -1,22 +1,31 @@
 #include "biblioteca.h"
 
-Jogador* criaJogador (Posicao start_pos)
+/**
+ * \brief Cria a entidade Jogador numa determinada posição.
+ * \return Apontador para a entidade Jogador. 
+*/
+Entidade* criaJogador (Posicao start_pos)
 {
-    Jogador* novoJogador = calloc(1, sizeof(Jogador));
+    Entidade* novoJogador = calloc(1, sizeof(Entidade));
 
     novoJogador->pos.y = start_pos.y;
     novoJogador->pos.x = start_pos.x;
     novoJogador->imagem = '@';
-    novoJogador->cor = COLOR_PAIR(COR_VISIVEL);
+    novoJogador->cor = COLOR_PAIR(COR_VISTA);
 
     return novoJogador;
 }
 
-void direcao (int input)
+/**
+ * \brief Altera as direção do Jogador usando as teclas numpad.
+ * \param tecla 
+ * 
+*/
+void direcao (int tecla)
 {
     Posicao nova_pos = { jogador->pos.y, jogador->pos.x };
 
-    switch(input)
+    switch(tecla)
     {
         //move up
         case 'w':
@@ -41,13 +50,19 @@ void direcao (int input)
     movimentaJogador(nova_pos);
 }
 
+/**
+ * \brief Movimenta o Jogador para uma determinada posição. 
+ * \param nova_pos
+*/
 void movimentaJogador(Posicao nova_pos)
 {
     if (mapa[nova_pos.y][nova_pos.x].podeAndar)
     {
         estadoNormalPeca(jogador);
+
         jogador->pos.y = nova_pos.y;
         jogador->pos.x = nova_pos.x;
+        
         modificaEstadoPeca(jogador);
     }
 }
