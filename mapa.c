1ee83cff70b1647b45329d81ba6658031919036a
liandrapereira <pliandra.xavier@gmail.com>
2023-05-16 16:46:03 +0100

    Não alterei nada.

--- mapa.c
+++ mapa.c
@@ -1,8 +1,8 @@
 #include "biblioteca.h"
 
 /**
- * \brief Cria o mapa adicionando peça por peça na memória alocada.
- * @return Apontador para o primeiro elemento da matriz ou ínício do mapa.
+ * \brief Cria todas as peças do mapa alocando memória para cada peça.
+ * @return Apontador para vetor de apontadores de cada peça do mapa.  // Ainda por verificar
 */
 Peca** criaMapaPecas() {
 
@@ -11,6 +11,7 @@ Peca** criaMapaPecas() {
     for (int y = 0; y < MAP_HEIGHT; y++) // y - comprimento do mapa
     {
         pecas[y] = calloc(MAP_WIDTH, sizeof(Peca));
+
         for (int x = 0; x < MAP_WIDTH; x++) // x - largura do mapa;
         {
             pecas[y][x].imagem = '#'; // # - Representa uma parede
@@ -19,41 +20,35 @@ Peca** criaMapaPecas() {
             pecas[y][x].transparente = false;
             pecas[y][x].visivel = false;
             pecas[y][x].vista = false;
-
         }
     }
-    return pecas;
+
+    return pecas; // Cria peça por peça 
 }
 
 /**
- * \brief
+ * \brief Constroi as salas e os corredores dentro do mapa. 
+ * \return Posição da primeira sala do mapa. 
 */
-Posicao setupMap(){
+Posicao constroiSalasMapa(){
 
-    int y, x, altura, largura, n_salas;
-    n_salas =  (rand() % 11) + 5;
-    // Room rooms[n_rooms];
-    Sala* salas = calloc(n_salas, sizeof(Sala));
-    Posicao pos_inicial;
+    int y, x, altura, largura;
+    int numero_salas = (rand() % 11) + 5; //O número máximo de salas é 15 --> Adicionar -- Entre 5 e 15
 
-    for (int i = 0; i < n_salas; i++)
+    Sala* salas = calloc(numero_salas, sizeof(Sala));
+    Posicao pos_SalaInicial;
+
+    for (int i = 0; i < numero_salas; i++)
     {
-        y = (rand() % (MAP_HEIGHT - 10)) + 1;
-        x = (rand() % (MAP_WIDTH - 20)) + 1;
-        altura = (rand() % 7) + 3;
-        largura = (rand() % 15) + 5;
+        y = (rand() % (MAP_HEIGHT - 15)) + 1; // 10 -- Tem de estar dentro do mapa
+        x = (rand() % (MAP_WIDTH - 25)) + 1;  // 20 -- Também 
+
+        altura = (rand() % 7) + 3; // Altura máxima é 9 - Entre 3 e 9
+        largura = (rand() % 15) + 5; // Largura máxima é 19 - Entre 5 e 19
+
         salas[i] = criaSala(y, x, altura, largura);
         adicionaSalaMapa(salas[i]);
-
-        /*
-        y = (rand() % (MAP_HEIGHT - 10)) + 1;
-        x = (rand() % (MAP_WIDTH - 20)) + 1;
-        height = (rand() % 7) + 3;
-        width = (rand() % 15) + 5;
-        rooms[i] = createRoom(y, x, height, width);
-        addRoomToMap(rooms[i]);
-        */
-
+        
         if (i > 0)
         {
             connectaCentroSalas(salas[i-1].centro, salas[i].centro);
@@ -61,17 +56,17 @@ Posicao setupMap(){
 
     }
 
-    pos_inicial.y = salas[0].centro.y;
-    pos_inicial.x = salas[0].centro.x;
+    pos_SalaInicial.y = salas[0].centro.y;
+    pos_SalaInicial.x = salas[0].centro.x;
 
     free(salas);
 
-    return pos_inicial;
+    return pos_SalaInicial; // Primeira sala criada no mapa
 
 }
 
 /**
- * \brief Liberta o espaço do mapa na memória.
+ * \brief Liberta o espaço do mapa alocado na memória.
 */
 void freeMap()
 {
