1e86a3360d72f79ebe0199f6292ccdda5a86a97c
liandrapereira <pliandra.xavier@gmail.com>
2023-05-16 16:43:54 +0100

    Alterei as dimensões do mapa.

--- main.c
+++ main.c
@@ -1,25 +1,28 @@
 #include "biblioteca.h"
 
-const int MAP_HEIGHT = 25;
-const int MAP_WIDTH = 100;
+const int MAP_HEIGHT = 25; //50
+const int MAP_WIDTH = 100; // 150
 
-Jogador* jogador;
+Entidade* jogador;
 Peca** mapa;
 
-int main(void)
-{
-    Posicao start_pos;
+
+int main()
+{   
+    Posicao pos_inicial;
     bool compatibleTerminal;
 
     compatibleTerminal = cursesSetup();
 
     if (compatibleTerminal)
-    {
-        srand(time(NULL));
+    {   
+        srand(time(NULL)); // Retorna uma semente diferente sempre que inicia-se o jogo.
 
-        mapa = criaMapaPecas();
-        start_pos = setupMap();
-        jogador = criaJogador(start_pos);
+        mapa = criaMapaPecas(); // Cria o mapa
+
+        pos_inicial = constroiSalasMapa(); // Posiciona o jogador na primeira sala criada
+
+        jogador = criaJogador(pos_inicial); //Desenha o jogador no mapa
 
         gameLoop();
 
