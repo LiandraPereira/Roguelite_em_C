e21a771a217ee06e482d601f3b2ed44f9a0b7c57
Janaina Buvana <a101534@alunos.uminho.pt>
2023-06-03 19:24:32 +0100

    Combate e sua implementações

--- combate.c
+++ combate.c
@@ -61,27 +61,26 @@ void ataque_monstro(Monstro *atacante, Monstro *alvo) { //criar um valor para o 
 double calcula_distancia(int x1, int y1, int x2, int y2){ //calcular a distância usando os pontos das coordenadas
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
-void perseguicao(Monstro *monstro, int jogador_x, int jogador_y){ //dados de perseguição do monstro
+void perseguicao(Monstro *monstro, int jogador_x, int jogador_y) { //dados de perseguição do monstro
     double distancia = calcula_distancia(monstro->x, monstro->y, jogador_x, jogador_y);//calcula a distancia entre o monstro e o jogador
 
     if (distancia > 1){//se a distancia for maior que 1, aproximar-se do jogador
         //calcula o movimento da direção
-        int dx = (int)round((jogador_x - monstro->x)/ distancia);
-        int dy = (int)round((jogador_y - monstro->y)/ distancia);
+        int dx = (int) round((jogador_x - monstro->x) / distancia);
+        int dy = (int) round((jogador_y - monstro->y) / distancia);
 
         monstro->y += dy; //atualizar a localização do monstro
         monstro->x += dx;
     }
 }
-
 int main() {// gerador de número
     srand(time(NULL));
 
-    Jogador jogador = {"Jogador", 100, 100, 10, 5, "Humano", 10, 10 };
+    Jogador jogador = {"Jogador", 100, 100, 10, 5, "Humano", 10, 10};
     Monstro goblin = {"Goblin", 30, 30, 6, 2, "Pequeno", 5, 5};
 
-    Espada espada_jogador = { "Espada", 8};
-    Faca faca_jogador = { "Faca", 5};
+    Espada espada_jogador = {"Espada", 8};
+    Faca faca_jogador = {"Faca", 5};
 
     int ch;
 
@@ -91,22 +90,20 @@ int main() {// gerador de número
     cbreak();
     noecho();
 
-
     printw("%s encontra um %s selvagem %s!\n", jogador.nome, goblin.monstro_tipo, goblin.nome);
     refresh();
     getch();
 
-    while (jogador.vida > 0 && goblin.hp_atual > 0){
+    while (jogador.vida > 0 && goblin.hp_atual > 0) {
 
         ataque_monstro(&goblin, &jogador); //ataque do monstro
 
-        printw("\nClique na tecla 'f' para atacar com uma faca e 'e' para atacar com uma espada. \n" )
+        printw("\nClique na tecla 'f' para atacar com uma faca e 'e' para atacar com uma espada. \n")
         refresh();
         ch = getch();
 
         if (ch == 'f' || ch == 'F') {
-            printw("\n%s ataca %s com uma %d!\n", jogador.nome, goblin.nome, faca_jogador.nome);
-            refresh();
+            printw("\n%s ataca %s com uma %s!\n", jogador.nome, goblin.nome, faca_jogador.nome);
             getch();
 
             int dano_faca = faca_jogador.potencia_do_ataque; //dano da faca
@@ -117,10 +114,8 @@ int main() {// gerador de número
             mvprintw(8, 0, "%s's HP: %d", jogador.nome, jogador.vida);
             refresh();
             getch();
-        }
-        else if (ch == 'e' || ch == 'E'){
+        } else if (ch == 'e' || ch == 'E') {
             printw("\n%s ataca %s com uma %s!\n", jogador.nome, goblin.nome, espada_jogador.nome);
-            refresh();
             getch();
 
             int dano_espada = espada_jogador.potencia_do_ataque; //dano da espada
@@ -142,24 +137,18 @@ int main() {// gerador de número
 
         if (ch == 'W' || ch == 'w'){
             jogador.y--;
-        }
-        else if (ch == 'S' ||ch == 's'){
+        } else if (ch == 'S' ||ch == 's'){
             jogador.y++;
-        }
-        else if (ch == 'A' || ch =='a'){
+        } else if (ch == 'A' || ch =='a'){
             jogador.x--;
-        }
-        else if (ch == 'D' || ch == 'd'){
+        } else if (ch == 'D' || ch == 'd'){
             jogador.x++;
-        }
-        else if (ch == 'q' || ch == 'Q'){
+        } else if (ch == 'q' || ch == 'Q'){
             break;
         }
         perseguicao(&goblin, jogador.x, jogador.y);
     }
 
-
     endwin();//exit ncurses and clean up
-
     return 0;
 }
